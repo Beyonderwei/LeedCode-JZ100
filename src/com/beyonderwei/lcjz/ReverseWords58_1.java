0165b7eeba92acb6a1dcd4eb7d17c743ccec8779
Beyonderwei <beyonderwei@gmail.com>
2021-03-01 20:59:35 +0800

    [feat] 翻转单词顺序

---
+++ src/com/beyonderwei/lcjz/ReverseWords58_1.java
@@ -0,0 +1,17 @@
+package com.beyonderwei.lcjz;
+
+public class ReverseWords58_1 {
+
+    public String reverseWords(String s) {
+        s = s.trim(); // 删除首尾空格
+        int j = s.length() - 1, i = j;
+        StringBuilder res = new StringBuilder();
+        while(i >= 0) {
+            while(i >= 0 && s.charAt(i) != ' ') i--; // 搜索首个空格
+            res.append(s.substring(i + 1, j + 1) + " "); // 添加单词  此时i处于空格处 j在单词最后一个字母，由于左闭右开，因此都要加一
+            while(i >= 0 && s.charAt(i) == ' ') i--; // 跳过单词间空格
+            j = i; // j 指向下个单词的尾字符
+        }
+        return res.toString().trim(); // 转化为字符串并返回
+    }
+}
