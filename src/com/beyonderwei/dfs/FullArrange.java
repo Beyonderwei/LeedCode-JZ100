21ac7da1b3cd963c369b31bd24016d0e6ce27695
Beyonderwei <beyonderwei@gmail.com>
2021-03-27 20:35:52 +0800

    [fix] DFS：引入求全排列记忆理解的方法

--- src/com/beyonderwei/dfs/FullArrange.java
+++ src/com/beyonderwei/dfs/FullArrange.java
@@ -4,35 +4,36 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 求数组的全排列，同样是套DFS框架，
- *
+ * 1. 只要有if判断满足条件的个数一定要加 return
+ * 2. 有start for循环中就不需要加入是否已经入栈的判断（if (stack.contains(arr[i])) continue;）
+ * 3. 子集和 组合  需要start 因为不能重复的添加（因为 [1, 2] 和 [2, 1]属于相同的子集或组合），而全排列的话没有start
+ * 4. 由于栈是先进后出，因此想要得到由前到后的子集、组合 需要从后往前遍历
  */
 
+// 求全排列：321 231 312 132 213 123    满足 stack.size() == 3 时一定要加return
 public class FullArrange {
 
-    private static List<List<Integer>> res = new LinkedList<>(); // 存放子集数据
+    private static LinkedList<LinkedList<Integer>> res = new LinkedList<>();
 
     public static void main(String[] args) {
-        LinkedList<Integer> stack = new LinkedList<>(); // 存储已有的路径
-        int[] arr = {0, 1, 2, 3, 4};
+        int[] arr = {1, 2, 3};
+        LinkedList<Integer> stack = new LinkedList<>();
         dfs(arr, stack);
         for (int i = 0; i < res.size(); i++) {
-            System.out.println(res.get(i).toString());
+            System.out.println(res.get(i));
         }
     }
 
-    // 通过DFS 解决全排列问题，输入一个不包含重复数字的数组，输出全排列
     public static void dfs(int[] arr, LinkedList<Integer> stack) {
-        // 求arr的全排列，长度与数组相等时才添加
-        if (stack.size() == arr.length) {
+        if (stack.size() == 3) {
             res.add(new LinkedList<>(stack));
             return;
         }
         for (int i = 0; i < arr.length; i++) {
             if (stack.contains(arr[i])) continue;
-            stack.add(arr[i]);
+            stack.push(arr[i]);
             dfs(arr, stack);
-            stack.removeLast();
+            stack.poll();
         }
     }
 }
