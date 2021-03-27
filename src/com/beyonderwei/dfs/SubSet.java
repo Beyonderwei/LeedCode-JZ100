ac70a1520c3e896e98d5e3a70e172f6d2f2eedda
Beyonderwei <beyonderwei@gmail.com>
2021-03-27 20:31:47 +0800

    [fix] DFS：引入记忆理解的方法

--- src/com/beyonderwei/dfs/SubSet.java
+++ src/com/beyonderwei/dfs/SubSet.java
@@ -1,33 +1,33 @@
 package com.beyonderwei.dfs;
 
 import java.util.LinkedList;
-import java.util.List;
 
 /**
- * 求数组的所有子集，直接套用DFS框架
+ * 1. 只要有if判断满足条件的个数一定要加 return
+ * 2. 有start for循环中就不需要加入是否已经入栈的判断（if (stack.contains(arr[i])) continue;）
+ * 3. 子集和 组合  需要start 因为不能重复的添加（因为 [1, 2] 和 [2, 1]属于相同的子集或组合），而全排列的话没有start
+ * 4. 由于栈是先进后出，因此想要得到由前到后的子集、组合 需要从后往前遍历
  */
-
+// 求数组元素的所有子集 [] [1] [2, 1] [3, 2, 1] [3, 1] [2] [3, 2] [3]
+// 如果想调换顺序只需要将 start 改为 arr.length - 1  dfs中的for循环从后向前遍历
 public class SubSet {
-
-    private static List<List<Integer>> res = new LinkedList<>(); // 存放子集数据
+    private static LinkedList<LinkedList<Integer>> res = new LinkedList<>();
 
     public static void main(String[] args) {
-        LinkedList<Integer> stack = new LinkedList<>(); // 存储已有的路径
-        int[] arr = {0, 1, 2, 3, 4};
-        dfs(arr, 0, stack);
+        int[] arr = {1, 2, 3};
+        LinkedList<Integer> stack = new LinkedList<>();
+        dfs(arr, stack, 0);
         for (int i = 0; i < res.size(); i++) {
-            System.out.println(res.get(i).toString());
+            System.out.println(res.get(i));
         }
     }
 
-    // 通过DFS 解决子集问题，输入一个不包含重复数字的数组，输出所有的子集
-    public static void dfs(int[] arr, int start, LinkedList<Integer> stack) {
-        // 求arr的全部子集，直接add
+    public static void dfs(int[] arr, LinkedList<Integer> stack, int start) {
         res.add(new LinkedList<>(stack));
         for (int i = start; i < arr.length; i++) {
-            stack.add(arr[i]);
-            dfs(arr, i + 1, stack);
-            stack.removeLast();
+            stack.push(arr[i]);
+            dfs(arr, stack, i + 1);
+            stack.poll();
         }
     }
 
